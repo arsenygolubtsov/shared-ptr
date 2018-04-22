3fccdc1a24d9168c35776790154d2221f0d148de
KadrusBAG <32191410+KadrusBAG@users.noreply.github.com>
2018-04-22 11:39:27 +0300

    Update scoped_ptr.cpp

--- tests/scoped_ptr.cpp
+++ tests/scoped_ptr.cpp
@@ -4,12 +4,10 @@
 #include "scoped_ptr.hpp"
 
 TEST_CASE("num1"){
-	scoped_ptr <int> ptr1;
-	ptr1.reset(new int(3));
+	scoped_ptr <int> ptr1(new int(3));
 	int *ptr_1 = ptr1.get();
 	
-	scoped_ptr <int> ptr2;
-	ptr1.reset(new int(6));
+	scoped_ptr <int> ptr2(new int(6));
 	int ptr_2 = *ptr2;
 	
 	REQUIRE(*ptr_1 == 3);
@@ -17,11 +15,9 @@ TEST_CASE("num1"){
 }
 
 TEST_CASE("num2"){
-	scoped_ptr <int> ptr1;
-	ptr1.reset(new int(3));
+	scoped_ptr <int> ptr1(new int(3));
 	
-	scoped_ptr <int> ptr2;
-	ptr1.reset(new int(6));
+	scoped_ptr <int> ptr2(new int(6));
 	
 	ptr1.swap(ptr2);
 	
@@ -30,4 +26,13 @@ TEST_CASE("num2"){
 	
 	REQUIRE(*ptr_1 == 6);
 	REQUIRE(ptr_2 == 3);	
+}
+
+TEST_CASE("num3"){
+	scoped_ptr <int> ptr1(new int(3));
+	ptr1.reset(new int(6));
+	
+	int ptr = ptr1.get();
+	
+	REQUIRE(ptr1 == 6);
 }
