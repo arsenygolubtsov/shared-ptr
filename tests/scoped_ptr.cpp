c4e7e251216c5cfb177a9f6b3d9537372784e732
KadrusBAG <32191410+KadrusBAG@users.noreply.github.com>
2018-04-22 11:14:28 +0300

    Update and rename tree.cpp to scoped_ptr.cpp

---
+++ tests/scoped_ptr.cpp
@@ -0,0 +1,33 @@
+#include <catch.hpp>
+#include <sstream>
+
+#include "scoped_ptr.hpp"
+
+TEST_CASE("num1"){
+	scoped_ptr <int> ptr1;
+	ptr1.reset(new int(3));
+	int *ptr_1 = ptr1.get();
+	
+	scoped_ptr <int> ptr2;
+	ptr1.reset(new int(6));
+	int ptr_2 = *ptr2;
+	
+	REQUIRE(*ptr_1 == 3);
+	REQUIRE(ptr_2 = 6);
+}
+
+TEST_CASE("num2"){
+	scoped_ptr <int> ptr1;
+	ptr1.reset(new int(3));
+	
+	scoped_ptr <int> ptr2;
+	ptr1.reset(new int(6));
+	
+	ptr1.swap(ptr2);
+	
+	int *ptr_1 = ptr1.get();
+	int ptr_2 = *ptr2;
+	
+	REQUIRE(*ptr_1 == 6);
+	REQUIRE(ptr_2 = 3);	
+}
