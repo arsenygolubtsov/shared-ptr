6e93108c57b56889988321572008ac244335293c
KadrusBAG <32191410+KadrusBAG@users.noreply.github.com>
2018-04-26 09:39:54 +0300

    Update and rename scoped_ptr.cpp to shared_ptr.cpp

--- tests/shared_ptr.cpp
+++ tests/shared_ptr.cpp
@@ -1,18 +1,18 @@
 #include <catch.hpp>
 #include <sstream>
 
-#include "scoped_ptr.hpp"
+#include "shared_ptr.hpp"
 
 TEST_CASE("NULL"){
-	scoped_ptr <int> ptr;
+	shared_ptr <int> ptr;
 	
 	REQUIRE (ptr.get() == nullptr);
 }
 TEST_CASE("num1"){
-	scoped_ptr <int> ptr1(new int(3));
+	shared_ptr <int> ptr1(new int(3));
 	int *ptr_1 = ptr1.get();
 	
-	scoped_ptr <int> ptr2(new int(6));
+	shared_ptr <int> ptr2(new int(6));
 	int ptr_2 = *ptr2;
 	
 	REQUIRE(*ptr_1 == 3);
@@ -20,9 +20,9 @@ TEST_CASE("num1"){
 }
 
 TEST_CASE("num2"){
-	scoped_ptr <int> ptr1(new int(3));
+	shared_ptr <int> ptr1(new int(3));
 	
-	scoped_ptr <int> ptr2(new int(6));
+	shared_ptr <int> ptr2(new int(6));
 	
 	ptr1.swap(ptr2);
 	
@@ -34,7 +34,7 @@ TEST_CASE("num2"){
 }
 
 TEST_CASE("num3"){
-	scoped_ptr <int> ptr1(new int(3));
+	shared_ptr <int> ptr1(new int(3));
 	ptr1.reset(new int(6));
 	
 	int *ptr = ptr1.get();
